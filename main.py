e3c94b89697311cec3ac1d7c2702f99d08f6d087
arjuuuuunnnnn <arjunbanur27@gmail.com>
2025-01-11 11:00:37 +0530

    FD generalised

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torchvision
+import math
+import matplotlib.pyplot as plt
 import torch
+import urllib
+import numpy as np
+import PIL
+from unet import UNet
 
-def forward_diffusion(x0, t, betas = torch.linspace(0, 1, 5)):
-    noise = torch.rand_like(x0)
-    alphas = 1 - betas
-    alpha_hat = torch.cumprod(alphas, axis=0)
-    alpha_hat_t = alpha_hat.gather(-1, t).reshape(-1, 1, 1, 1)
+device = 'cpu'
 
-    mean = alpha_hat_t.sqrt() * x0
-    variance = torch.sqrt(1-alpha_hat_t) * noise
+def get_sample_image()->PIL.Image.Image:
+    url = ""
+    filename = "sample.jpg"
+    urllib.request.urlretrieve(url, filename)
+    return PIL.Image.open(filename)
 
-    return mean+variance, noise
+class DiffusionModel:
+    def __init__(self, start_schedule=0.001, end_schedule=0.02, timesteps=1000):
+        self.start_schedule = start_schedule
+        self.end_schedule = end_schedule
+        self.timesteps = timesteps
+
+        self.betas = torch.linspace(start_schedule, end_schedule, timesteps)
+        self.alphas = 1 - self.betas
+        self.alphas_cumprod = torch.cumprod(self.alphas, axis=0)
+        
+
+    def forward_diffusion(x0, t, betas = torch.linspace(0, 1, 5)):
+        noise = torch.rand_like(x0)
+        sqrt_alphas_cumprod_t = self.get_index_from_list(self.alphas_cumprod.sqrt(), t, x0.shape)
+        sqrt_one_minus_alphas_cumprod_t = self.get_index_from_list(torch.sqrt(1. - self.alphas_cumprod), t, x0.shape)
+
+        mean = sqrt_alphas_cumprod_t.to(device) * x0.to(device)
+        variance = sqrt_one_minus_alphas_cumprod_t.to(device) * noise.to(device)
+
+        xt = mean + variance
+        
+        return xt, noise.to(device)
+
+    @staticmethod
+    def get_index_from_list(values, t, x_shape):
+        batch_size = t.shape[0]
+        result = values.gather(-1, t.cpu())
+
+        return result.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
 
 
+        
--- rough.py
+++ rough.py
@@ -28,4 +28,14 @@ variance = torch.sqrt(1 - result) * noise
 
 x_t = mean + variance
 
+def forward_diffusion(x0, t, betas = torch.linspace(0, 1, 5)):
+    noise = torch.rand_like(x0)
+    alphas = 1 - betas
+    alpha_hat = torch.cumprod(alphas, axis=0)
+    alpha_hat_t = alpha_hat.gather(-1, t).reshape(-1, 1, 1, 1)
 
+    mean = alpha_hat_t.sqrt() * x0
+    variance = torch.sqrt(1-alpha_hat_t) * noise
+    x_t = mean+variance
+
+    return x_t, noise
